a30be6bd219a520ccfc8c0259096c7a6b537e0e3
alviny <alviny@kth.se>
2025-11-11 23:28:38 +0100

    finish writing file reader, begin work on code execution part

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,23 @@
 
 use std::{collections::HashMap, env::args, fs, io::{self}, process::exit};
 
-#[derive(Clone, PartialEq, Eq, Hash)]
+#[derive(Clone, PartialEq, Eq, Hash, Debug)]
 enum Register{
     Zero,
-    Stdio,
     Iter0,
     Iter1,
-    Cond0,
-    Cond1,
+    Cond,
+
     Temp0,
     Temp1,
     Temp2,
+    Temp3,
+
     Arg0,
     Arg1,
     Arg2,
+    Arg3,
+
     Save0,
     Save1,
     Save2,
@@ -26,9 +29,10 @@ enum Register{
 enum InterpreterError{
     NotAlnumError,
     SyntaxError,
+    ParseError,
 
 }
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 struct CPU {
     registers: HashMap<Register, u16>
 }
@@ -36,17 +40,17 @@ impl CPU {
     fn new() -> CPU {
         CPU {registers: HashMap::from([
             (Register::Zero, 0),
-            (Register::Stdio, 0),
             (Register::Iter0, 0),
             (Register::Iter1, 0),
-            (Register::Cond0, 0),
-            (Register::Cond1, 0),
+            (Register::Cond, 0),
             (Register::Temp0, 0),
             (Register::Temp1, 0),
             (Register::Temp2, 0),
+            (Register::Temp3, 0),
             (Register::Arg0, 0),
             (Register::Arg1, 0),
             (Register::Arg2, 0),
+            (Register::Arg3, 0),
             (Register::Save0, 0),
             (Register::Save1, 0),
             (Register::Save2, 0),
@@ -54,39 +58,78 @@ impl CPU {
 
         ])}
     }
-    fn execute_operation(&mut self, op: String) {
+
+    // register instructions
+    fn assign_plus(&mut self, reg1: Register, reg2: Register, reg3: Register) {
         todo!()
-    } 
+    }
+    fn assign_minus(&mut self, reg1: Register, reg2: Register, reg3: Register) {
+        todo!()
+    }
+
+    //immediate instructions, 2 registers
+    fn immassign_plus(&mut self, reg1: Register, reg2: Register, imm: i8) {
+        todo!()
+    }
+    
 }
 
 fn parse_file(file_path: &String) -> Result<Vec<String>, InterpreterError> {
     // failsafe: check if file ending is correct. If not, error
-    let operations = match *(file_path.split('.').collect::<Vec<_>>().last().unwrap()) == "anm" {
+    let mut operations = match *(file_path.split('.').collect::<Vec<_>>().last().unwrap()) == "anm" {
         true => fs::read_to_string(file_path).expect("Unable to read input file"),
         false => return Err(InterpreterError::NotAlnumError),
     };
 
-
     // split by newline
-    // remove comments on each line
-
+    let mut operations_split:Vec<String> = operations.split("\n")
+    .collect::<Vec<&str>>()
+    .into_iter()
+    .map(|x| 
+        x.strip_suffix("\r")
+        .unwrap_or(x)
+        // we remove comments here, in the same map function
+        .split('#')
+        .collect::<Vec<&str>>()[0]
+        .trim()
+        .to_string()
+        
+    )
+    .collect::<Vec<String>>();
     // purge blank lines
+    operations_split.retain(|x| !x.is_empty());
+    println!("{:?}", operations_split);
 
     // we now have a vector full of statements. 
     // check each statement for disallowd characters (Capital letters, commas, period, )
     
     //return it
-    
+    Ok(operations_split)
 }
 fn main() {
     let args: Vec<String> = args().collect();
     let mut cpu = CPU::new();
 
-    let statements = parse_file(&args[1]);
+    let statements_result = parse_file(&args[1]);
+    // An error here means you haven't entered a file with the correct file ending.
+    let statements = match statements_result {
+        Ok(vec) => vec,
+        Err(error) => panic!("Interpreter Error: {error:?}"),
+    };
     println!("{:?}", statements);
-    // exit(0);
-    for i in statements {
-        cpu.execute_operation(i);
+    
+    let mut line_counter: usize= 1;
+    while line_counter <= statements.len().try_into().unwrap() {
+        let op = statements[line_counter -1].split_whitespace().collect::<Vec<&str>>();
+
+        let execution_result: Result = match op[0]{
+            _ => 
+        }
+
+
+        line_counter += 1;
     }
+
+
     println!("---Program finished running (Exit code: 0)---");
 }
