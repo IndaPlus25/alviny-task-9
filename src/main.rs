df829843809afa9a4c717ab22fe3dd6db30a1cc5
alviny <alviny@kth.se>
2025-11-12 22:26:03 +0100

    update syntax error mesage to be more clear

--- src/main.rs
+++ src/main.rs
@@ -295,7 +295,7 @@ fn main() {
     let mut line_counter: usize= 1;
     while line_counter <= statements.len() {
         let op = statements[line_counter -1].split_whitespace().collect::<Vec<&str>>();
-        
+
         let execution_result: Result<i16, InterpreterError> = match op[0]{
             "assign" => if op[2] == "to" {
                 let reg1 = get_register(op[1]).expect("BadRegisterError: No register of that name!");
@@ -371,7 +371,7 @@ fn main() {
         let jump_number: i16 = match execution_result {
             Ok(_) => {execution_result.ok().unwrap()},
             Err(error) => {
-                println!("{error:?} at line {line_counter}, registers dumped ({cpu:?})");
+                println!("{error:?} at instruction number {line_counter}, registers dumped ({cpu:?})");
                 println!("---Program finished running (Exit code: 1)---");
                 exit(1);
             }
